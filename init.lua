05d8d57ff2a028b0dd0b1d1449d2ec15177a1030
rusty407 <sandroamiridze93@gmail.com>
2025-09-26 23:11:02 -0400

    added couple things like remaps and numbers on left side

--- init.lua
+++ init.lua
@@ -1,6 +1,3 @@
 require("config.lazy")
-vim.opt.guicursor = {
-  "n-v-c:block",                      -- normal, visual, command: solid block
-  "i-ci:blinkon500-blinkoff500",     -- insert, insert-command: blinking block
-  "r-cr:blinkon500-blinkoff500",     -- replace, command-replace: blinking block
-}
+require("config.set")
+require("config.remap")
---
+++ lua/config/remap.lua
@@ -0,0 +1,18 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- chenge the specific word you select 
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+
+
+
+
+
+
